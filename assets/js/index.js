88d5bcb868d212d1fab9a26878b5519e20de84de
Dylan Vorster <dylanvorster.dv@gmail.com>
2023-06-03 15:30:27 -0600

    gallery fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ require("../css/index.scss");
 require("typeface-source-sans-pro");
 const hljs = require("highlight.js");
 require("highlight.js/styles/atom-one-dark.css");
+import "photoswipe/style.css";
+import PhotoSwipe from "photoswipe";
 
 document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector(
@@ -11,5 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
 	document.querySelectorAll("pre code").forEach((el) => {
 		console.log(el);
 		hljs.highlightElement(el);
+	});
+
+	document.querySelectorAll(".kg-gallery-container").forEach((el) => {
+		const items = [];
+
+		el.querySelectorAll("img").forEach((img, index) => {
+			items.push({
+				srcset: img.srcset,
+				src: img.src,
+				width: img.getAttribute("width"),
+				height: img.getAttribute("height"),
+			});
+			img.addEventListener("click", () => {
+				const pswp = new PhotoSwipe({
+					dataSource: items,
+					index: index,
+				});
+				pswp.init();
+			});
+		});
 	});
 });
