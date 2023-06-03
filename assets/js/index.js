9a9addb30d79458d8f3b704addc5a280d8984fdd
Dylan Vorster <dylanvorster.dv@gmail.com>
2023-06-02 22:20:14 -0600

    improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,15 @@
-require("../css/index.scss")
+require("../css/index.scss");
+require("typeface-source-sans-pro");
+const hljs = require("highlight.js");
+require("highlight.js/styles/atom-one-dark.css");
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('#copyright').innerHTML = `Copyright ${new Date().getFullYear()}`;
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector(
+		"#copyright"
+	).innerHTML = `Copyright ${new Date().getFullYear()}`;
+
+	document.querySelectorAll("pre code").forEach((el) => {
+		console.log(el);
+		hljs.highlightElement(el);
+	});
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-const path = require('path');
+const path = require("path");
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-    entry: './assets/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'assets/built'),
-        filename: 'darkfox.js',
-        assetModuleFilename: '[file][ext][query]'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.otf/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.png/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.scss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
+	entry: "./assets/js/index.js",
+	mode: prod ? "production" : "development",
+
+	output: {
+		path: path.resolve(__dirname, "assets/built"),
+		filename: "darkfox.js",
+		assetModuleFilename: "[file][ext][query]",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(otf|woff2|woff)/,
+				type: "asset/resource",
+			},
+			{
+				test: /\.png/,
+				type: "asset/resource",
+			},
+			{
+				test: /\.scss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
 };
